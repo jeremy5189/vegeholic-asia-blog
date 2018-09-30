ff91cb1da1515cd18b4c4a565bb86b74767521ec
Jeremy Yen <jeremy5189@gmail.com>
2018-09-30 14:20:34 +0800

    Init commit

---
+++ wp-login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="zh-TW" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://ogp.me/ns/fb#">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-TW" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://ogp.me/ns/fb#">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>登入 &lsaquo; Vegeholic 全植食尚 &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/wp-content/uploads/2016/08/cropped-chia1-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2016/08/cropped-chia1-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2016/08/cropped-chia1-180x180.jpg" />
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2016/08/cropped-chia1-270x270.jpg" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-zh-tw">
+		<div id="login">
+		<h1><a href="http://tw.wordpress.org/" title="本站採用 WordPress 建置" tabindex="-1">本站採用 WordPress 建置</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">使用者名稱或電子郵件地址<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">密碼<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> 記住我</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="登入" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">忘記密碼？</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; 返回 Vegeholic 全植食尚</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
